84c3348a6a68cb3f3e473b8b69db13f52ef47363
Francisco <francisco.santanna@gmail.com>
2025-05-29 17:10:06 -0300

    - expr -> parser

--- src/parser.lua
+++ src/parser.lua
@@ -1,4 +1,3 @@
-require "expr"
 require "prim"
 
 -------------------------------------------------------------------------------
@@ -34,8 +33,6 @@ function accept_err (str, tag)
 end
 
 -------------------------------------------------------------------------------
-
-parser = parser_5_bin
 
 function parser_list (sep, clo, f)
     assert(sep or clo)
@@ -114,3 +111,110 @@ function parser_main ()
     accept_err('<eof>')
     return { tag='block', es=es }
 end
+
+-------------------------------------------------------------------------------
+
+-- 6_out : v --> f     f <-- v    v where {...}    v thus {...}
+-- 5_bin : a + b
+-- 4_pre : -a    :T [...]
+-- 3_met : v->f    f<-v
+-- 2_suf : v[0]    v.x    v.1    v.(:T).x    f()
+-- 1_prim
+
+local function is_prefix (e)
+    return (
+        e.tag == 'tag'    or
+        e.tag == 'acc'    or
+        e.tag == 'nat'    or
+        e.tag == 'call'   or
+        e.tag == 'index'  or
+        e.tag == 'parens'
+    )
+end
+
+function parser_2_suf (pre)
+    local e = pre or parser_1_prim()
+    local ok = (TK0.lin==TK1.lin) and is_prefix(e)
+    if not ok then
+        return e
+    end
+
+    local ret = nil
+    if e.tag=='tag' and (check'(' or check'[') then
+        local t = parser()
+        local f = { tag='acc', tk={tag='id',str="atm_tag"} }
+        ret = { tag='call', f=f, args={e,t} }
+    elseif accept('(') then
+        local args = parser_list(',', ')', parser)
+        accept_err(')')
+        ret = { tag='call', f=e, args=args }
+--[[
+    elseif check('[') or check(nil,'str') then
+        local v = parser_prim_1()
+        ret = { tag='call', f=e, args={v} }
+]]
+    elseif accept('[') then
+        local idx = parser()
+        accept_err(']')
+        ret = { tag='index', t=e, idx=idx }
+    elseif accept('.') then
+        local id = accept_err(nil,'id')
+        id = { tag='tag', str=':'..id.str }
+        local idx = { tag='tag', tk=id }
+        ret = { tag='index', t=e, idx=idx }
+    else
+        -- nothing consumed, not a suffix
+        return e
+    end
+
+    return parser_2_suf(ret)
+end
+
+local function method (f, e, pre)
+    if f.tag == 'call' then
+        if pre then
+            table.insert(f.args, 1, e)
+        else
+            f.args[#f.args+1] = e
+        end
+        return f
+    else
+        return { tag='call', f=f, args={e} }
+    end
+end
+
+function parser_3_met (pre)
+    local e = pre or parser_2_suf()
+    if accept('->') then
+        return parser_3_met(method(parser_2_suf(), e, true))
+    elseif accept('<-') then
+        return method(e, parser_3_met(parser_2_suf()), false)
+    else
+        return e
+    end
+end
+
+function parser_4_pre ()
+    local ok = check(nil,'op') and contains(OPS.unos, TK1.str)
+    if not ok then
+        return parser_3_met()
+    end
+    local op = accept_err(nil,'op')
+    local e = parser_4_pre()
+    return { tag='uno', op=op, e=e }
+end
+
+function parser_5_bin (pre)
+    local e1 = pre or parser_4_pre()
+    local ok = check(nil,'op') and contains(OPS.bins, TK1.str)
+    if not ok then
+        return e1
+    end
+    local op = accept_err(nil,'op')
+    if pre and pre.op.str ~= op.str then
+        err(op, "binary operation error : use parentheses to disambiguate")
+    end
+    local e2 = parser_4_pre()
+    return parser_5_bin { tag='bin', op=op, e1=e1, e2=e2 }
+end
+parser = parser_5_bin
