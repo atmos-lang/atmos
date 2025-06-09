6288f272b71a4dd3cf6a510ca06d3ea1004371cd
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-06-09 14:23:34 -0300

    - TODO: loader, searcher

--- src/exec.lua
+++ src/exec.lua
@@ -3,6 +3,33 @@ require "lexer"
 require "parser"
 require "coder"
 
+function atm_searcher (name)
+    local path = package.path:gsub('%?%.lua','?.atm'):gsub('init%.lua','init.atm')
+    local f, err = package.searchpath(name, path)
+    if not f then
+        return f, err
+    end
+    return atm_loadfile, f
+end
+
+function atm_load (src, file)
+    init()
+    lexer_init(file, src)
+    lexer_next()
+    local main = parser_main()
+    local lua = [[
+        local atm_as_lua = function () ]] .. coder_stmts(main.blk.es) .. [[ end
+        require 'aux'
+        require 'runtime'
+        return atm_exec(file, main)
+            "]] .. file .. [[",
+            ]] .. string.format('%q', coder_stmts(main.blk.es)) .. [[
+        )
+    ]]
+
+    return load(src, file)
+end
+
 function exec_file (file)
     local f = assert(io.open(file))
     local src = f:read('*a')
