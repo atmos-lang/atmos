fc5d8a91cb6ac8705bf1628f9fee8e6b66c44eb2
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-08-19 23:25:21 -0300

    - test: require

--- tst/cmd.lua
+++ tst/cmd.lua
@@ -3,9 +3,11 @@ require "atmos.lang.aux"
 -- VOID / ARGS / ...
 
 do
+    print("Testing...", "void-01.atm")
     local v = io.popen("atmos cmd/void-01.atm"):read('*a')
     assertx(v, "")
 
+    print("Testing...", "args-01.atm")
     local v = io.popen("atmos cmd/args-01.atm 1 2 3"):read('*a')
     assertx(v, "1\t2\t3\n")
 end
@@ -13,9 +15,19 @@ end
 -- TEST
 
 do
+    print("Testing...", "test-01.atm")
     local v = io.popen("atmos cmd/test-01.atm"):read('*a')
     assertx(v, "1\n3\n")
 
+    print("Testing...", "test-01.atm")
     local v = io.popen("atmos cmd/test-01.atm --test"):read('*a')
     assertx(v, "1\n2\n3\n")
+end
+
+-- REQUIRE
+
+do
+    print("Testing...", "require-01.atm")
+    local v = io.popen("atmos cmd/require-01.atm"):read('*a')
+    assertx(v, "20\n")
 end
