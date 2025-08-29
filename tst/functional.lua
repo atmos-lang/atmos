f7088b82e8804c0592ea1745eaba93893698000f
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-08-29 17:26:40 -0300

    - OK: functional -> streams

--- tst/functional.lua
+++ tst/functional.lua
@@ -35,7 +35,9 @@ local src = [[
       "Caleb",
       "Dennis"
     }
-    val names_with_b = S.fr_vector(names)::filter(\{it::find("[Bb]")})::to_vector()
+    val names_with_b = S.fr_vector(names)
+        ::filter(\{it::find("[Bb]")})
+        ::to_vector()
     xprint(names_with_b)
 ]]
 print("Testing...", "func 3")
@@ -50,9 +52,10 @@ local src = [[
       "Caleb",
       "Dennis"
     }
-    val names_with_b = S.fr_vector(names)::filter(\{it::find("[Bb]")})::
-        map(string.upper)::
-        to_vector()
+    val names_with_b = S.fr_vector(names)
+        ::filter(\{it::find("[Bb]")})
+        ::map(string.upper)
+        ::to_vector()
     xprint(names_with_b)
 ]]
 print("Testing...", "func 4")
@@ -61,9 +64,9 @@ assertx(out, "#{BEATRICE, CALEB}\n")
 
 local src = [[
     val S = require "atmos.lang.streams"
-    S.range(1, 10)::    ;; run through all the numbers from 1 to 10 (inclusive)
+    S.fr_range(1, 10)::    ;; run through all the numbers from 1 to 10 (inclusive)
       filter(\{(it%2)==0})::  ;; take only even numbers
-      foreach(print)   ;; run print for every value individually
+      to_each(print)   ;; run print for every value individually
 ]]
 print("Testing...", "func 5")
 local out = atm_test(src)
@@ -71,11 +74,11 @@ assertx(out, "2\n4\n6\n8\n10\n")
 
 local src = [[
     val S = require "atmos.lang.streams"
-    val names = @{"hellen", "oDYSseuS", "aChIlLeS", "PATROCLUS"}
+    val names = #{"hellen", "oDYSseuS", "aChIlLeS", "PATROCLUS"}
     val fix_case = func (name) {
       name::sub(1,1)::upper() ++ name::sub(2)::lower()
     }
-    loop name in S.map(names, fix_case) {
+    loop name in S.fr_vector(names)::map(fix_case) {
       print(name)
     }
 ]]
@@ -85,8 +88,8 @@ assertx(out, "Hellen\nOdysseus\nAchilles\nPatroclus\n")
 
 local src = [[
     val S = require "atmos.lang.streams"
-    val numbers = S.range(10,15)
-    val sum = numbers::reduce(\(acc,new){acc+new}, 0)
+    val numbers = S.fr_range(10,15)
+    val sum = numbers::to_acc(0) <- \(acc,new){acc+new}
     print(sum)
 ]]
 print("Testing...", "func 7")
@@ -95,28 +98,28 @@ assertx(out, "75\n")
 
 local src = [[
     val S = require "atmos.lang.streams"
-    val numbers = @{2, 1, 3, 4, 7, 11, 18, 29}
+    val numbers = #{2, 1, 3, 4, 7, 11, 18, 29}
 
     val is_even = \{(it % 2) == 0}
-    xprint <-- S.filter(numbers, is_even)::to_table()
+    xprint <-- S.fr_vector(numbers)::filter(is_even)::to_vector()
 
-    xprint <-- S.filter(numbers, \{(it % 2) == 0})::to_table()
+    xprint <-- S.fr_vector(numbers)::filter(\{(it % 2) == 0})::to_table()
 ]]
 print("Testing...", "func 8")
 local out = atm_test(src)
-assertx(out, "{2, 4, 18}\n{2, 4, 18}\n")
+assertx(out, "#{2, 4, 18}\n{2, 4, 18}\n")
 
 local src = [[
     val S = require "atmos.lang.streams"
-    val matrix = @{
-      @{1, 2, 3}, ;; first element of matrix
-      @{4, 5, 6}, ;; second element of matrix
-      @{7, 8, 9}  ;; third element of matrix
+    val matrix = #{
+      #{1, 2, 3}, ;; first element of matrix
+      #{4, 5, 6}, ;; second element of matrix
+      #{7, 8, 9}  ;; third element of matrix
     }
     ;; map will iterate through each row, and the lambda
     ;; indexes each to retrieve the first element
-    xprint <-- S.map(matrix, \{it[1]})::to_table()
+    xprint <-- S.fr_vector(matrix)::map(\{it[1]})::to_vector()
 ]]
 print("Testing...", "func 9")
 local out = atm_test(src)
-assertx(out, "{1, 4, 7}\n")
+assertx(out, "#{2, 5, 8}\n")
