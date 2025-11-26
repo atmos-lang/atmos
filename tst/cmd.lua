af4755e2bfb2822f0aca91066c493663af88b0fa
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-11-26 06:42:23 -0300

    - add test for previous commit

--- tst/cmd.lua
+++ tst/cmd.lua
@@ -4,27 +4,34 @@ require "atmos.lang.exec"
 
 do
     print("Testing...", "void-01.atm")
-    local v = io.popen("atmos cmd/void-01.atm"):read('*a')
+    local v = io.popen("atmos 2>&1 cmd/void-01.atm"):read('*a')
     assertx(v, "")
 
     print("Testing...", "args-01.atm")
-    local v = io.popen("atmos cmd/args-01.atm 1 2 3"):read('*a')
+    local v = io.popen("atmos 2>&1 cmd/args-01.atm 1 2 3"):read('*a')
     assertx(v, "1\t2\t3\n")
 
     print("Testing...", "comm-01.atm")
-    local v = io.popen("atmos cmd/comm-01.atm"):read('*a')
+    local v = io.popen("atmos 2>&1 cmd/comm-01.atm"):read('*a')
     assertx(v, "")
+end
+
+-- ERROR
+do
+    print("Testing...", "error-01.atm")
+    local v = io.popen("atmos 2>&1 cmd/error-01.atm"):read('*a')
+    assertfx(v, "cmd%/error%-01.atm : line 1 : near '%*' : expected expression")
 end
 
 -- TEST
 
 do
     print("Testing...", "test-01.atm")
-    local v = io.popen("atmos cmd/test-01.atm"):read('*a')
+    local v = io.popen("atmos 2>&1 cmd/test-01.atm"):read('*a')
     assertx(v, "1\n3\n")
 
     print("Testing...", "test-01.atm")
-    local v = io.popen("atmos cmd/test-01.atm --test"):read('*a')
+    local v = io.popen("atmos 2>&1 cmd/test-01.atm --test"):read('*a')
     assertx(v, "1\n2\n3\n")
 end
 
@@ -32,6 +39,6 @@ end
 
 do
     print("Testing...", "require-01.atm")
-    local v = io.popen("atmos cmd/require-01.atm"):read('*a')
+    local v = io.popen("atmos 2>&1 cmd/require-01.atm"):read('*a')
     assertx(v, "20\n")
 end
