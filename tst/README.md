5fcdf09a78ddf0beb2786ad6f068245f2966ed24
Francisco Sant'Anna <francisco.santanna@gmail.com>
2026-01-10 09:38:20 -0300

    ...

--- tst/README.md
+++ tst/README.md
@@ -17,5 +17,4 @@ lua-atmos
 
 ```bash
 LUA_PATH="../../../lua-atmos/f-streams/?/init.lua;../../../lua-atmos/atmos/?.lua;../../../lua-atmos/atmos/?/init.lua;;" lua5.4 all.lua
-lua5.4 cmd.lua  # requires atmos installed
 ```
--- tst/cmd.lua
+++ tst/cmd.lua
@@ -4,22 +4,22 @@ require "atmos.lang.exec"
 
 do
     print("Testing...", "void-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/void-01.atm"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/void-01.atm"):read('*a')
     assertx(v, "")
 
     print("Testing...", "args-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/args-01.atm 1 2 3"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/args-01.atm 1 2 3"):read('*a')
     assertx(v, "1\t2\t3\n")
 
     print("Testing...", "comm-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/comm-01.atm"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/comm-01.atm"):read('*a')
     assertx(v, "")
 end
 
 -- ERROR
 do
     print("Testing...", "error-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/error-01.atm"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/error-01.atm"):read('*a')
     assertfx(v, "cmd%/error%-01.atm : line 1 : near '%*' : expected expression")
 end
 
@@ -27,11 +27,11 @@ end
 
 do
     print("Testing...", "test-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/test-01.atm"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/test-01.atm"):read('*a')
     assertx(v, "1\n3\n")
 
     print("Testing...", "test-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/test-01.atm --test"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/test-01.atm --test"):read('*a')
     assertx(v, "1\n2\n3\n")
 end
 
@@ -39,6 +39,6 @@ end
 
 do
     print("Testing...", "require-01.atm")
-    local v = io.popen("atmos 2>&1 cmd/require-01.atm"):read('*a')
+    local v = io.popen("../atmos 2>&1 cmd/require-01.atm"):read('*a')
     assertx(v, "20\n")
 end
